8776e8c977a72280c4fe256c090d148083836307
unknown <ziparaujo@gmail.com>
2024-06-04 15:34:01 -0300

    primeiro exercicio

---
+++ src/projeto/AbonoSalario.java
@@ -0,0 +1,23 @@
+package projeto;
+
+import java.util.Scanner;
+
+public class AbonoSalario {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		
+		float salario, abono, novoSalario;
+		
+		System.out.println("Digite o seu salário: ");
+		salario = read.nextFloat();
+		System.out.println("Digite o abono: ");
+		abono = read.nextFloat();
+		
+		novoSalario = salario + abono;
+		
+		System.out.println("Seu novo salário é: R$" + novoSalario);
+		read.close();
+	}
+
+}
